44455196bbe2f92808625f671d0e94bd3fd33d1b
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-04-10 10:27:05 -0500

    change Obtain instance behavior control to be impacted by setting attributes after setup_dworshak_managers() instantiation, rather than instancing with the Obtain() clas directly

--- src/pipeline_eds/context.py
+++ src/pipeline_eds/context.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 from pathlib import Path
 from dworshak_prompt import setup_dworshak_managers
-from dworshak_prompt import Obtain, InterruptBehavior, PromptMode
+from dworshak_prompt import InterruptBehavior, PromptMode
 
 DEFAULT_DWORSHAK_DIR = str(Path.home() / ".pipeline-eds")
 dworshak_managers = setup_dworshak_managers(dir = globals().get("DEFAULT_DWORSHAK_DIR"))
@@ -10,8 +10,15 @@ dworshak_root_dir = dworshak_managers["root"]
 secret_mngr = dworshak_managers["secret"]
 config_mngr = dworshak_managers["config"]
 env_mngr = dworshak_managers["env"]
-#obtain_mngr = dworshak_managers["obtain"]
+obtain_mngr = dworshak_managers["obtain"]
 
+# --- Control behavior of the Obtain class instance, beyong the default. 
+# Impact the attibutes which are instatiated differently by default, to your desired behavior
+obtain_mngr.interrupt_behavior = InterruptBehavior.EXIT
+obtain_mngr.interface_priority = [PromptMode.WEB,PromptMode.GUI,PromptMode.CONSOLE]
+obtain_mngr.interface_avoid = {}
+
+'''# Alternative example 
 # Add extra control features, compared to the standard obtain manager, but leverage the assigned pathing.
 obtain_mngr = Obtain(
     config_path=config_mngr.path, 
@@ -19,3 +26,4 @@ obtain_mngr = Obtain(
     interrupt_behavior=InterruptBehavior.EXIT,
     interface_priority=[PromptMode.WEB,PromptMode.GUI,PromptMode.CONSOLE]
     )
+'''
