42cdf3279b4f9cbfc15ecffb37bab6bb3b140fc2
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-05-07 15:12:03 -0500

    inject eds_rest_api_sub_path = api/v1 to disk with dworshak config set, but position it to allow users to edit manually

--- src/pipeline_eds/api/eds/rest/config.py
+++ src/pipeline_eds/api/eds/rest/config.py
@@ -1,6 +1,7 @@
 # src/pipeline_eds/api/eds/rest/config.py
 from __future__ import annotations
 from typing import Dict
+from dworshak_config import DworshakConfig
 from dworshak_prompt import Obtain, InterruptBehavior, PromptMode
 
 #from pipeline_eds.security_and_config import SecurityAndConfig, get_base_url_config_with_prompt, not_enough_info
@@ -10,6 +11,8 @@ obtain = Obtain(
     interrupt_behavior=InterruptBehavior.EXIT,
     interface_priority=[PromptMode.WEB,PromptMode.GUI,PromptMode.CONSOLE]
     )
+
+config_mngr = DworshakConfig()
 
 def get_rest_api_url(base_url: str | None = None,
                         eds_rest_api_port: int | None = 43080, 
@@ -41,15 +44,15 @@ def get_eds_rest_api_credentials(plant_name: str, overwrite: bool = False, forge
     idcs_to_iess_suffix = eds_config.get_idcs_to_iess_suffix(plant_name=plant_name, overwrite=overwrite)
     zd = eds_config.get_zd(plant_name=plant_name, overwrite=overwrite)
     
-    eds_rest_api_port = obtain.config(service = "eds",item = f"rest_api_port_{plant_name}", message = f"Enter {plant_name} EDS REST API port", overwrite=overwrite, suggestion = "43084").value
-    eds_rest_api_sub_path = obtain.config(service = "eds",item = f"rest_api_sub_path_{plant_name}", message = f"Enter {plant_name} EDS REST API sub path", overwrite=overwrite, suggestion = "api/v1").value
     
+    eds_rest_api_port = obtain.config(service = "eds",item = f"rest_api_port_{plant_name}", message = f"Enter {plant_name} EDS REST API port", overwrite=overwrite, suggestion = "43084").value
+    
+    config_mngr.set(service = "eds",item = f"rest_api_sub_path_{plant_name}", value = "api/v1", overwrite=False) # pass value to users machine, but allow them to edit it manually if it exists by leaving overwrite as False
+    eds_rest_api_sub_path = obtain.config(service = "eds",item = f"rest_api_sub_path_{plant_name}", message = f"Enter {plant_name} EDS REST API sub path", overwrite=overwrite, suggestion = "api/v1").value
+    eds_rest_api_sub_path = str(eds_rest_api_sub_path).rstrip("/").lstrip("/").replace(r"\\","/").lower()
+
     username = eds_security.get_username(plant_name=plant_name, overwrite=overwrite)
     password = eds_security.get_password(plant_name=plant_name, overwrite=overwrite)
-    
-    #if not all([username, password]):
-    #    raise CredentialsNotFoundError(f"API credentials for '{plant_name}' not found. Please run the setup utility.")
-    eds_rest_api_sub_path = str(eds_rest_api_sub_path).rstrip("/").lstrip("/").replace(r"\\","/").lower()
 
     # EDS REST API Pattern: url = f"http://{url}:43084/api/v1" # assume EDS patterna and port http and append api/v1 if user just puts in an IP
     
