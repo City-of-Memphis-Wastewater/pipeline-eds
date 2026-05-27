a29ef7be7fd7107aa7f3c3021501a0b44edf8cda
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-05-27 18:59:53 -0500

    rwmove tauri multimodal example app from codebase

--- src/example/tauri_multimodal_app/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'dist',  // <-- look for index.html here # no this is dumb, thats a build folder not a templates folder
-  build: {
-    outDir: '../dist-build', // output folder for Vite build, can be src-tauri/../dist-build
-    emptyOutDir: true
-  }
-});
