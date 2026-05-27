f30110ccd6eb9f410207cd230659ffce9bdd8ee3
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-05-27 17:54:03 -0500

    remove detritis

--- src/pipeline_eds/decorators.py
+++ src/pipeline_eds/decorators.py
@@ -1,6 +1,5 @@
 from __future__ import annotations # Delays annotation evaluation, allowing modern 3.10+ type syntax and forward references in older Python versions 3.8 and 3.9
 import logging
-import inspect
 from functools import wraps
 
 
