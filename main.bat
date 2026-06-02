7c9f241831551f08d8cb2ae5e1e6b3ceb9e04d0e
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-06-02 07:19:25 -0500

    adjust time manager

--- main.bat
+++ main.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Log current timestamp with TimeManager formatting
-uv run python -c "from pipeline_eds.time_manager import TimeManager; print('----', TimeManager.now().as_formatted_date_time(), '----')" >> logs/daemon_log.txt
+uv run python -c "from pipeline_eds.time_manager import TimeManager; print('----', TimeManager(TimeManager.now()).as_formatted_date_time(), '----')" >> logs/daemon_log.txt
 
 REM Show uv version
 uv --version
