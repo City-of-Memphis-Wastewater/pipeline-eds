3cdaf52e74006f25b6a054fd2ba5eeaf93045824
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-09-21 10:13:51 -0500

    great, kivy is now building the CLI, using the __main__ entry point

--- src/frontend_kivy/main.py
+++
@@ -1,8 +0,0 @@
-# src/frontend_kivy/main.py
-import sys
-from pipeline_eds.cli import app
-
-# this only works if the lanch_kivy_app is the default no-arg command of the app
-
-if __name__ == "__main__":
-    app()
